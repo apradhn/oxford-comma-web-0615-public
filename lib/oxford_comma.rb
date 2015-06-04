d045a276ab90d70fd17a8b2be3182fe4d1b19ace
apradhn <abhijit.pradhan6@gmail.com>
2015-06-04 13:51:27 -0400

    all done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
 def oxford_comma(array)
-
+  list_of_words = []
+  array.each.with_index do |word, i|
+    if array.size == 1
+      list_of_words << word
+    elsif i < array.size-1
+        if array.size == 2
+          list_of_words << word
+        else
+          list_of_words << word + ","  
+        end       
+    elsif i == array.size-1
+        list_of_words << "and " + word      
+    end 
+  end
+  list_of_words.join(" ")
 end
